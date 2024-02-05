8519fe52f3def20c40f9d520983959f0ad38c9a5
achensetyawati <achenzetya@gmail.com>
2024-02-05 14:20:03 +0700

    add getsize

--- Com.Everyday.Service.Core.WebApi/Controllers/v1/BasicControllers/SizeController.cs
+++ Com.Everyday.Service.Core.WebApi/Controllers/v1/BasicControllers/SizeController.cs
@@ -17,9 +17,33 @@ namespace Com.DanLiris.Service.Core.WebApi.Controllers.v1.BasicControllers
     public class SizeController : BasicController<SizeService, SizeModel, SizeViewModel, CoreDbContext>
     {
         private new static readonly string ApiVersion = "1.0";
+        SizeService service;
 
         public SizeController(SizeService service) : base(service, ApiVersion)
         {
+            this.service = service;
+        }
+
+        [HttpGet("sizename")]
+        public async Task<IActionResult> GetSizeName(string code)
+        {
+            try
+            {
+                List<SizeModel> Data = await service.GetSizeName(code);
+
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.OK_STATUS_CODE, General.OK_MESSAGE)
+                    .Ok(Data);
+
+                return Ok(Result);
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
         }
     }
 }
